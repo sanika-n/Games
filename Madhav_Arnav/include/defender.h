553039be1aa09a715ffd6e186ed16063f7287e0b
Arnav-Thorat <arnavt.official@gmail.com>
2025-04-11 22:02:38 +0530

    Defenders updated

--- Madhav_Arnav/include/defender.h
+++ Madhav_Arnav/include/defender.h
@@ -2,18 +2,26 @@
 #define DEFENDER_H
 
 #include <string>
+#include <vector>
 #include "enemy.h"
 
 class Defender {
 public:
-    Defender(std::string type, int damage);
-    void attack(Enemy& target) const;
-
+    Defender(std::string type, int damage, int range);
+    void attack(std::vector<Enemy*> enemies) const;
     std::string getName() const;
+    int getRange() const;
 
 private:
     std::string type;
     int attackPower;
+    int range;
 };
 
 #endif
+
+// add type of defenders: pea shooter, bonk choy, etc.
+// attack function takes the vector of pointers of enemies and it should find the closest enemy, and attacks it in one turn
+// and attack it
+// add kill function that will remove the enemy from the vector of pointers
+
